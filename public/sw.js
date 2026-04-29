4e0aeb29a83be03e5221ae6602f28b7ba2f9ed05
kanah0724-wq <kana.h0724@gmail.com>
2026-04-29 16:49:15 +0900

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'mizu-v' + Date.now();
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
